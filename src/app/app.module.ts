7fea022721d8c3b069d8e976e19f406aae38fab6
Roy Joseph <roy@drawout.dk>
2023-11-08 17:24:25 +0530

    Rearranged the folder structure.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
 import { FormsModule } from '@angular/forms';
-import { ConvertToSpacesPipe } from './shared/convertToSpaces/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
+import { ConvertToSpacesPipe } from './shared/convertToSpacesPipe/convert-to-spaces.pipe';
+import { StarComponent } from './shared/starComponent/star.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe, StarComponent, ProductDetailComponent],
   imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
